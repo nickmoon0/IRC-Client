5e2873e0855a72765698482c722715855686460b
Nicholas Moon <nick@eliteyeet.com>
2019-12-08 16:14:20 +0800

    Added constructors to server

--- include/server.h
+++ include/server.h
@@ -1,14 +1,27 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+// Standard headers
+#include <string>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+// Project headers
+
 class server {
 public:
 
-	server();
-
+	server(std::string serverAddress, std::string port);
+	server(std::string serverAddress);
+	
 private:
 
-	
+	// Server details
+	const char DEFAULT_PORT[5] = "6667";
+	std::string port;
+	std::string serverAddress;
 
 };
 
--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,13 @@
  * Constructor
  */
 
-server::server() {
-	
+server::server(std::string serverAddress, std::string port) {
+	this->serverAddress = serverAddress;
+	this->port = port;
+}
+
+server::server(std::string serverAddress) {
+	this->serverAddress = serverAddress;
+
+	this->port = DEFAULT_PORT;
 }
