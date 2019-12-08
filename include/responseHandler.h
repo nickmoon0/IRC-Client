1be83ad0691fd528929863aaa90971ef4d26ece6
Nicholas Moon <nick@eliteyeet.com>
2019-12-08 19:02:04 +0800

    Added ServerReceiver struct

--- include/responseHandler.h
+++ include/responseHandler.h
@@ -1,11 +1,40 @@
 #ifndef RESPONSEHANDLER_H
 #define RESPONSEHANDLER_H
 
+// Standard headers
+#include <string>
+#include <vector>
+#include <thread>
+
+// Project headers
+#include "interface.h"
+#include "server.h"
+
+struct ServerReceiver {
+
+	server* ss_server;
+	std::thread* ss_receiveThread;
+
+};
+
 class responseHandler {
 public:
 
-private:
+	responseHandler(interface* mainInterface);
 	
+	int addServer(server* s);
+
+	void handleResponse(int socket);
+
+private:
+
+	// Server stuff
+	std::vector<ServerReceiver*>* serverList;
+
+
+	// Misc
+	interface* mainInterface;
+
 };
 
 #endif
--- src/responseHandler.cpp
+++ src/responseHandler.cpp
@@ -1,1 +1,27 @@
 #include "responseHandler.h"
+
+/*
+ * Constructor
+ */
+
+responseHandler::responseHandler(interface* mainInterface) {
+	this->mainInterface = mainInterface;
+	this->serverList = new std::vector<ServerReceiver*>();
+}
+
+/*
+ * Getting server ready
+ */
+
+int responseHandler::addServer(server* s) {
+	
+}
+
+/*
+ * Handling response
+ */
+
+void responseHandler::handleResponse(int socket) {
+
+}
+
