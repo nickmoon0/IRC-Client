8555e4b37d46edb31cc8449e3b01367e3fc8cde9
Nicholas Moon <nick@eliteyeet.com>
2019-12-09 18:57:07 +0800

    Added addServer function

--- include/responseHandler.h
+++ include/responseHandler.h
@@ -1,20 +1,41 @@
 #ifndef RESPONSEHANDLER_H
 #define RESPONSEHANDLER_H
 
+// Standard headers
+#include <vector>
+#include <thread>
+#include <string>
+
 // Project headers
 #include "user.h"
 #include "interface.h"
 #include "server.h"
 
+struct ServerListener {
+	server* sl_server;
+	std::thread *sl_listenerThread;
+	bool sl_connectionActive;
+};
+
 class responseHandler {
 public:
 
+	// Constructor
 	responseHandler(user* currentUser, interface* mainInterface);
+
+	// Adding server
+	int addServer(std::string serverAddress, std::string port);
 
 private:
 	
+	// Listener function
+	void listenerFunc();
+
+
 	user* currentUser;
 	interface* mainInterface;
+
+	std::vector<ServerListener*>* serverList;
 
 };
 
