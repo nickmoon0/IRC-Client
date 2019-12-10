a13b1a57fc93cd161150a70e276aea50f1ed2942
Nicholas Moon <nick@eliteyeet.com>
2019-12-10 23:00:40 +0800

    Added some skeleton functions

--- include/requestHandler.h
+++ include/requestHandler.h
@@ -1,11 +1,39 @@
 #ifndef REQUESTHANDLER_H
 #define REQUESTHANDLER_H
 
+// Standard headers
+#include <string>
+
+// Project headers
+#include "interface.h"
+#include "user.h"
+
+struct commands {
+	const std::string NICK = "nick";
+	const std::string JOIN = "join"; // can be used for channel or server depending on what follows command
+	
+	const std::string PARK = "part"; // Used to leave channel
+	const std::string LEAVE = "leave"; // Used to leave server
+	const std::string QUIT = "quit"; // Used to quit client
+};
+
 class requestHandler {
 public:
 
+	requestHandler(user* currentUser, interface* mainInterface);
+	~requestHandler();
+
+	void handleInput(std::string input);
+
 private:
-	
+
+	// Handling data
+	const char COMMAND_PREFIX = '.';
+
+	// Misc
+	user* currentUser;
+	interface* mainInterface;
+
 };
 
 #endif
--- src/requestHandler.cpp
+++ src/requestHandler.cpp
@@ -1,1 +1,20 @@
 #include "requestHandler.h"
+
+/*
+ * Constructor/Destructor
+ */
+
+requestHandler::requestHandler(user* currentUser, interface* mainInterface) {
+	this->currentUser = currentUser;
+	this->mainInterface = mainInterface;
+}
+
+/*
+ * Handling
+ */
+
+void requestHandler::handleInput(std::string input) {
+	if (input.at(0) == COMMAND_PREFIX) {
+		
+	}
+}
