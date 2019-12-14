4e16ecbe1a6572edc5c08e2473f149da9b9a77fc
Nicholas Moon <nick@eliteyeet.com>
2019-12-15 06:22:19 +0800

    Added listener class

---
+++ include/listener.h
@@ -0,0 +1,29 @@
+#ifndef LISTENER_H
+#define LISTENER_H
+
+// Standard headers
+#include <thread>
+#include <string>
+
+// Project headers
+#include "server.h"
+#include "responseHandler.h"
+#include "user.h"
+
+class listener {
+public:
+
+	listener(server* serv, user* currentUser);
+	int start();
+
+private:
+
+	// Important details
+	server* serv;
+	user* currentUser;
+
+	// Thread stuff
+	std::thread *listeningThread;
+};
+
+#endif
---
+++ src/listener.cpp
@@ -0,0 +1,50 @@
+#include "listener.h"
+
+/*
+ * Constructor
+ */
+
+listener::listener(server* serv, user* currentUser) {
+	this->serv = serv;
+	this->currentUser = currentUser;
+
+	this->listeningThread = nullptr;
+}
+
+/*
+ * Listening
+ */ 
+
+int listener::start() {
+	listeningThread = new std::thread(&listener::listen, this);
+}
+
+int listener::listen() {
+	// Get the socket and create message buffer
+	int socket = serv->getSocket();
+	char msgBuffer[serv->MAX_MESSAGE_LEN];
+
+	// While the connection is open
+	while (serv->getConnectionOpen()) {
+		
+		// Clear the buffer
+		memset(msgBuffer, 0, sizeof msgBuffer);
+		
+		// Receive a message
+		int bytesReceived = recv(socket, msgBuffer, serv->MAX_MESSAGE_LEN, 0);
+	
+		// Check that a message was successfully received
+		if (bytesReceived <= 0) {
+			// fatal error occured or 
+			return -1;
+		}
+
+		// Convert buffer to string
+		std::string strMsg = msgBuffer;
+
+		// Handle string and clear string-buffer
+		respHandler->handleResponse(strMsg);
+		strMsg.clear();
+	}
+	return 0;
+}
